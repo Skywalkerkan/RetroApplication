4029e983a85d8c10bb8b85794b3cbed6713171c9
Erkan Coşar <117943189+Skywalkerkan@users.noreply.github.com>
2024-07-25 14:29:23 +0300

    Firebase Configuration ViewModel

--- RetroApp/View/MainView/MainView.swift
+++ RetroApp/View/MainView/MainView.swift
@@ -6,50 +6,70 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct MainView: View {
-    let items = ["Item 1", "Item 2", "Item 3", "Item 4"]
+    @StateObject private var viewModel = MainViewModel()
     
     var body: some View {
         NavigationView {
-            ZStack(alignment: .bottomTrailing) {
-                List(items, id: \.self) { item in
-                    PanelCell(panelName: item)
-                }
-                .navigationTitle("My List")
-                .navigationBarItems(
-                    leading: Button(action: {
-                        print("line")
-                    }) {
-                        Image(systemName: "line.3.horizontal")
-                    },
-                    trailing: HStack {
-                        Button(action: {
-                            print("Bildiri")
-                        }) {
-                            Image(systemName: "bell")
-                        }
-                        Button(action: {
-                            print("Settings")
-                        }) {
-                            Image(systemName: "gear")
-                        }
+            ZStack {
+                if viewModel.items.isEmpty {
+                    VStack {
+                        Spacer()
+                        Text("No Items")
+                            .font(.title)
+                            .foregroundColor(.gray)
+                        Spacer()
                     }
-                )
-                
-                NavigationLink(destination: CreateBoardView()) {
-                    Image(systemName: "plus")
-                        .foregroundColor(.white)
-                        .frame(width: 50, height: 50)
-                        .background(Color.cyan)
-                        .clipShape(Circle())
+                } else {
+                    List(viewModel.items) { item in
+                        PanelCell(panelName: item.title)
+                    }
                 }
-                .padding(16)
+                
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        NavigationLink(destination: CreateBoardView()) {
+                            Image(systemName: "plus")
+                                .foregroundColor(.white)
+                                .frame(width: 50, height: 50)
+                                .background(Color.cyan)
+                                .clipShape(Circle())
+                        }
+                        .padding(16)
+                    }
+                }
+            }
+            .navigationTitle("My List")
+            .navigationBarItems(
+                leading: Button(action: {
+                    print("line")
+                }) {
+                    Image(systemName: "line.3.horizontal")
+                },
+                trailing: HStack {
+                    Button(action: {
+                        print("Bildiri")
+                    }) {
+                        Image(systemName: "bell")
+                    }
+                    Button(action: {
+                        print("Settings")
+                    }) {
+                        Image(systemName: "gear")
+                    }
+                }
+            )
+            .onAppear {
+                viewModel.fetchItems()
+               // viewModel.addItem(RetroItem(title: "Sprint1", description: "Description", category: .toDo))
             }
         }
     }
 }
+
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
