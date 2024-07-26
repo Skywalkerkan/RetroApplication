9e374d512e8c93f9722d25099b7c7f6ecfbb177d
Erkan Coşar <117943189+Skywalkerkan@users.noreply.github.com>
2024-07-26 15:01:58 +0300

    BoardViewModel and Board View feature

--- RetroApp/View/CreateBoardView/CreateBoardView.swift
+++ RetroApp/View/CreateBoardView/CreateBoardView.swift
@@ -8,63 +8,71 @@
 import SwiftUI
 
 struct CreateBoardView: View {
+    
     @State private var textFieldText: String = ""
     @State private var showAlert = false
     @State private var alertMessage: String = ""
+    @State private var navigateToBoardView = false
     
     @ObservedObject var viewModel = CreateBoardViewModel()
     
     var body: some View {
         
-        NavigationView {
-            VStack {
-                VStack(alignment: .leading) {
-                    Text("Pano İsmi")
-                        .font(.title)
-                        .padding(.top, 16)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    TextField("Enter board name", text: $textFieldText)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.top, -4)
-                    
-                    HStack {
-                        Text("Pano Arkaplanı")
-                        Spacer()
-                        Rectangle()
-                            .cornerRadius(4)
-                            .frame(width: 30, height: 30)
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: 40)
-                }
-                .padding(.horizontal, 16)
+        VStack {
+            VStack(alignment: .leading) {
+                Text("Pano İsmi")
+                    .font(.title)
+                    .padding(.top, 16)
+                    .frame(maxWidth: .infinity, alignment: .leading)
                 
-                List(viewModel.listItems) { item in
-                    PanelCell(panelName: item.name)
-                }
-                Spacer()
+                TextField("Enter board name", text: $textFieldText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.top, -4)
                 
-                Button(action: {
-                    let item = RetroItem(title: textFieldText, description: "description", category: .done)
-                    //viewModel.addItem(item)
-                //   viewModel.addPanel()
-                    viewModel.fetchPanel()
-                    if let errorMessage = viewModel.errorMessage {
-                        alertMessage = errorMessage
-                        showAlert = true
-                    }
-                }) {
-                    Text("Kaydet")
-                        .frame(width: 150, height: 40)
-                        .foregroundColor(.white)
-                        .background(Color.cyan)
-                        .cornerRadius(8)
+                HStack {
+                    Text("Pano Arkaplanı")
+                    Spacer()
+                    Rectangle()
+                        .cornerRadius(4)
+                        .frame(width: 30, height: 30)
                 }
-                .padding(.bottom, 20)
+                .frame(maxWidth: .infinity, maxHeight: 40)
             }
-            .alert(isPresented: $showAlert) {
-                Alert(title: Text("Pano Eklenemedi"), message: Text(alertMessage), dismissButton: .default(Text("Tamam")))
+            .padding(.horizontal, 16)
+            
+            List(viewModel.listItems) { item in
+                PanelCell(panelName: item.name)
             }
+            Spacer()
+            
+            Button(action: {
+                let item = RetroItem(title: textFieldText, description: "description", category: .done)
+                //viewModel.addItem(item)
+                //viewModel.addPanel()
+                viewModel.fetchPanel()
+                if let errorMessage = viewModel.errorMessage {
+                    alertMessage = errorMessage
+                    showAlert = true
+                } else {
+                    navigateToBoardView = true
+                }
+            }) {
+                Text("Kaydet")
+                    .frame(width: 150, height: 40)
+                    .foregroundColor(.white)
+                    .background(Color.cyan)
+                    .cornerRadius(8)
+            }
+            .padding(.bottom, 20)
+            
+            NavigationLink(
+                destination: BoardView().navigationBarTitleDisplayMode(.inline),
+                isActive: $navigateToBoardView,
+                label: { EmptyView() }
+            )
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text("Pano Eklenemedi"), message: Text(alertMessage), dismissButton: .default(Text("Tamam")))
         }
         .navigationTitle("Pano Oluştur")
     }
